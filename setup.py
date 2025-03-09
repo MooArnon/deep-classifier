de9de07dde7e93962911975ffcbeb51d573e4023
MooArnon <oomarnon.000@gmail.com>
2025-03-09 08:46:36 +0700

    [init-project] add dependencies

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ with open("requirements.txt", "r", encoding="utf-8") as f:
 setup(
     name="deep_classifier",
     version="0.1.0",
-    author="Your Name",
-    author_email="your.email@example.com",
-    description="A feature engineering package for deep learning-based asset classification.",
+    author="Arnon Phongsiang",
+    author_email="arnon.phongsiang@gmail.com",
+    description="A package for deep learning-based asset classification.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/deep_classifier",  # update this URL
+    url="https://github.com/MooArnon/deep_classifier",  # update this URL
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
